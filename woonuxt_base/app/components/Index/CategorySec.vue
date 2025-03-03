206c2bce2cbfa70e51e184516aab0e2c1c9660f1
Boaz <webevolution13@gmail.com>
2025-03-03 17:47:44 +0100

    upDate
    front end

--- woonuxt_base/app/components/Index/CategorySec.vue
+++ woonuxt_base/app/components/Index/CategorySec.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup>
-const { data } = await useAsyncGql('getProductCategories');
-const productCategories = data.value.productCategories?.nodes as ProductCategory[];
-
+// Prop-ként fogadjuk a kategóriákat a szülőtől
+defineProps<{
+  categories: ProductCategory[]
+}>();
 </script>
 
 <template>
-    <main class="container">
-        <div v-if="productCategories?.length"
-            class="grid grid-cols-2 gap-4 my-6 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
-            <CategoryCard v-for="(category, i) in productCategories" :key="i" :node="category"
-                :image-loading="i <= 2 ? 'eager' : 'lazy'" />
-        </div>
-    </main>
+  <main class="container">
+    <div v-if="categories?.length"
+        class="grid grid-cols-2 gap-4 my-6 md:grid-cols-3 lg:gap-8 xl:grid-cols-4">
+        <CategoryCard v-for="(category, i) in categories" :key="i" :node="category"
+            :image-loading="i <= 2 ? 'eager' : 'lazy'" />
+    </div>
+  </main>
 </template>
